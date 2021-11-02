b83600b1e02b67fea127d3b2fbbe65401378f506
Daniel-Mustafa <83565485+Daniel-Mustafa@users.noreply.github.com>
2021-11-02 18:32:32 -0300

    Projetos Criados

---
+++ backend/src/test/java/com/mustafatecnologia/mtvendas/MtvendasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.mustafatecnologia.mtvendas;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MtvendasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
