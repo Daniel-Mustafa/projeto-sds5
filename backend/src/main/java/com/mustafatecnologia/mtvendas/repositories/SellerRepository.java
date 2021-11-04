f84e6b9ca3b517799ce94a7cc02bf46c1227e1a6
Daniel-Mustafa <83565485+Daniel-Mustafa@users.noreply.github.com>
2021-11-04 01:00:51 -0300

    Layers

---
+++ backend/src/main/java/com/mustafatecnologia/mtvendas/repositories/SellerRepository.java
@@ -0,0 +1,9 @@
+package com.mustafatecnologia.mtvendas.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.mustafatecnologia.mtvendas.entities.Seller;
+
+public interface SellerRepository extends JpaRepository<Seller, Long>{
+
+}
---
+++ backend/src/main/java/com/mustafatecnologia/mtvendas/service/SellerService.java
@@ -0,0 +1,24 @@
+package com.mustafatecnologia.mtvendas.service;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+import com.mustafatecnologia.mtvendas.dto.SellerDTO;
+import com.mustafatecnologia.mtvendas.entities.Seller;
+import com.mustafatecnologia.mtvendas.repositories.SellerRepository;
+
+@Service
+public class SellerService {
+	
+	@Autowired
+	private SellerRepository repository;
+	
+	public List<SellerDTO> findAll() {
+		List<Seller> result = repository.findAll();
+		return result.stream().map(x -> new SellerDTO(x)).collect(Collectors.toList());
+	}
+
+}
