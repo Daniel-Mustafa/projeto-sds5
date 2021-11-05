a315d46a139e0003675c799ac752c77c3cfa6a9d
Daniel-Mustafa <83565485+Daniel-Mustafa@users.noreply.github.com>
2021-11-05 02:32:08 -0300

    Group by search

--- backend/src/main/java/com/mustafatecnologia/mtvendas/controllers/SaleController.java
+++ backend/src/main/java/com/mustafatecnologia/mtvendas/controllers/SaleController.java
@@ -1,5 +1,7 @@
 package com.mustafatecnologia.mtvendas.controllers;
 
+import java.util.List;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
@@ -9,6 +11,8 @@ import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.mustafatecnologia.mtvendas.dto.SaleDTO;
+import com.mustafatecnologia.mtvendas.dto.SaleSuccessDTO;
+import com.mustafatecnologia.mtvendas.dto.SaleSumDTO;
 import com.mustafatecnologia.mtvendas.service.SaleService;
 
 @RestController
@@ -23,5 +27,17 @@ public class SaleController {
 		Page<SaleDTO> list = service.findAll(pageable);
 		return ResponseEntity.ok(list);
 		
+	}
+	
+	@GetMapping(value = "/amount-by-seller")
+	public ResponseEntity<List<SaleSumDTO>> amountGroupedBySeller() {
+		List<SaleSumDTO> list = service.amountGroupedBySeller();
+		return ResponseEntity.ok(list);
+	}
+	
+	@GetMapping(value = "/success-by-seller")
+	public ResponseEntity<List<SaleSuccessDTO>> successGroupedBySeller() {
+		List<SaleSuccessDTO> list = service.successGroupedBySeller();
+		return ResponseEntity.ok(list);
 	}
 }
